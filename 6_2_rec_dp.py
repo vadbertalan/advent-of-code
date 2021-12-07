6523e93bc6aa294fcb2fdf8f56a0438a6b10800b
Vad Bertalan <vadbertalan@yahoo.com>
2021-12-07 09:09:13 +0200

    day 6 add description

--- 6_2_rec_dp.py
+++ 6_2_rec_dp.py
@@ -1,3 +1,13 @@
+# --- Part Two - --
+
+# Suppose the lanternfish live forever and have unlimited food and space. Would they take over the entire ocean?
+
+# After 256 days in the example above, there would be a total of 26984457539 lanternfish!
+
+# How many lanternfish would there be after 256 days?
+
+# Your puzzle answer was 1732731810807.
+
 data_str = """3,4,3,1,2"""
 data_str = """1,3,4,1,5,2,1,1,1,1,5,1,5,1,1,1,1,3,1,1,1,1,1,1,1,2,1,5,1,1,1,1,1,4,4,1,1,4,1,1,2,3,1,5,1,4,1,2,4,1,1,1,1,1,1,1,1,2,5,3,3,5,1,1,1,1,4,1,1,3,1,1,1,2,3,4,1,1,5,1,1,1,1,1,2,1,3,1,3,1,2,5,1,1,1,1,5,1,5,5,1,1,1,1,3,4,4,4,1,5,1,1,4,4,1,1,1,1,3,1,1,1,1,1,1,3,2,1,4,1,1,4,1,5,5,1,2,2,1,5,4,2,1,1,5,1,5,1,3,1,1,1,1,1,4,1,2,1,1,5,1,1,4,1,4,5,3,5,5,1,2,1,1,1,1,1,3,5,1,2,1,2,1,3,1,1,1,1,1,4,5,4,1,3,3,1,1,1,1,1,1,1,1,1,5,1,1,1,5,1,1,4,1,5,2,4,1,1,1,2,1,1,4,4,1,2,1,1,1,1,5,3,1,1,1,1,4,1,4,1,1,1,1,1,1,3,1,1,2,1,1,1,1,1,2,1,1,1,1,1,1,1,2,1,1,1,1,1,1,4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,2,5,1,2,1,1,1,1,1,1,1,1,1"""
 
@@ -5,8 +15,7 @@ data = list(map(int, data_str.split(',')))
 
 total_days = 256
 
-# DP matrix
-dpm = {}
+dpm = {}  # DP matrix
 
 
 def spawn(timer, days_passed, parent, print_indent=''):
@@ -37,7 +46,6 @@ def spawn(timer, days_passed, parent, print_indent=''):
 
 
 counts = list(map(lambda timer: spawn(timer, 0, -9999), data))
-print(counts)
 count = sum(counts)
 
 print(count)
